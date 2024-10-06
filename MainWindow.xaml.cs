1759212e90483135c54322a3e5e224065fb131e4
Jazer <saltanhayrliev2006@gmail.com>
2024-10-07 00:22:16 +0300

    add MySqlTable to DataGrid

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace crud
 {
@@ -20,10 +23,20 @@ namespace crud
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection sqlConnection = null;
         public MainWindow()
         {
             InitializeComponent();
+            Load_Grid();
             clear.Click += Clear_Click;
+            insert.Click += Insert_Click;
+            //Form1_Load();
+            
+        }
+
+        private void Insert_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void ClearData() {
@@ -35,6 +48,26 @@ namespace crud
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             ClearData();
+        }
+        /*private void Form1_Load()
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString);
+            sqlConnection.Open();
+            if(sqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Установлено!");
+            }
+        }*/
+        public void Load_Grid()
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("select * from FirstTable",sqlConnection);
+            DataTable dt = new DataTable();
+            sqlConnection.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            //sqlConnection.Close();
+            dataGrid.ItemsSource = dt.DefaultView;
         }
     }
 }
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NULL, 
+    [Age] NCHAR(50) NULL, 
+    [City] NCHAR(50) NULL, 
+    [Gender] NCHAR(50) NULL
+)
